08ba6736427cd6a92f393ac3d69dc8d89fbe36da
Hemant Kushwaha <hk8181815252@gmail.com>
2026-07-22 12:19:18 +0530

    chore: add eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+// @ts-check
+
+import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig(
+    {
+        ignores: ['dist/**', 'node_modules/**', 'eslint.config.mjs'],
+    },
+    {
+        files: ['**/*.{js,ts}'],
+        extends: [
+            js.configs.recommended,
+            tseslint.configs.recommendedTypeChecked,
+        ],
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+            },
+        },
+        rules: {
+            'no-console': 'warn',
+            'no-unused-vars': 'error',
+            eqeqeq: 'warn',
+        },
+    },
+);
