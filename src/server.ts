c18ca6123e91c9cd79b9c19dbeae4c632d462555
Hemant Kushwaha <hk8181815252@gmail.com>
2026-07-22 14:19:16 +0530

    chore: add express setup

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,14 @@
+import app from './app.js';
 import { Config } from './config/index.js';
 
-function welcome(name: string): void {
-    console.log(`Welcome ${name}`);
+const startServer = () => {
+    const PORT = Config.PORT;
+    try {
+        app.listen(PORT, () => console.log(`server started on port ${PORT}`));
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+};
 
-    // const user = {
-    //     name: 'Hemant',
-    // };
-
-    // console.log(user.name);
-    console.log(Config.PORT);
-}
-
-welcome('Hemant');
+startServer();
