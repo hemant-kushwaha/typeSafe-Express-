22b1c9165e109d8c396803d371df3a0ef0061542
Hemant Kushwaha <hk8181815252@gmail.com>
2026-07-22 14:51:54 +0530

    chore: add logger setup

---
+++ src/config/logger.ts
@@ -0,0 +1,39 @@
+import winston from 'winston';
+import { Config } from './index.js';
+
+const logger = winston.createLogger({
+    level: 'info',
+    defaultMeta: {
+        serviceName: 'app service',
+    },
+    transports: [
+        new winston.transports.File({
+            level: 'info',
+            dirname: 'logs',
+            filename: 'combined.log',
+            format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.json(),
+            ),
+        }),
+        new winston.transports.File({
+            level: 'error',
+            dirname: 'logs',
+            filename: 'error.log',
+            format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.json(),
+            ),
+        }),
+        new winston.transports.Console({
+            level: 'info',
+            format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.json(),
+            ),
+            silent: Config.NODE_ENV == 'test',
+        }),
+    ],
+});
+
+export default logger;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import app from './app.js';
 import { Config } from './config/index.js';
+import logger from './config/logger.js';
 
 const startServer = () => {
     const PORT = Config.PORT;
     try {
-        app.listen(PORT, () => console.log(`server started on port ${PORT}`));
+        app.listen(PORT, () => {
+            logger.error('testing error log');
+            logger.info('Server Listen on port ', { port: PORT });
+        });
     } catch (error) {
         console.error(error);
         process.exit(1);
